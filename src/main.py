d6c043b6aa0dd7c18d679fe67e592cf0a8136c3b
Sergey <sergionik03@gmail.com>
2026-04-28 14:22:07 +0000

    add entry point

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from scripts.train_lstm import train
+from evulating.evulate_lstm import evulate
 
 if __name__ == "__main__":
-    train()
+#    train('configs/config.yaml')
+    evulate('configs/config.yaml', 'result_models/lstm_model_20.pt')
