ba942e349263f7993a9929d472dad4fa5391cdfa
Sergey <sergionik03@gmail.com>
2026-04-29 16:23:37 +0000

    add argparse for various actions and models

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-from scripts.train_lstm import train
-from evulating.evulate_lstm import evulate
+import argparse
 
-if __name__ == "__main__":
-#    train('configs/config.yaml')
-    evulate('configs/config.yaml', 'result_models/lstm_model_20.pt')
+parser = argparse.ArgumentParser()
+
+parser.add_argument("action", help="train, evulate or compare")
+parser.add_argument("--model_arch", help="lstm, bert or None")
+parser.add_argument("--config_file")
+parser.add_argument("--model_path")
+
+args = parser.parse_args()
+
+
+if args.action == "compare":
+    from evulating.compare_models import compare_models
+    compare_models("configs/config.yaml")
+
+if args.model_arch == "lstm":
+    from scripts.train_lstm import train
+    from evulating.evulate_lstm import evulate
+    
+    if args.action == "train":
+        train(args.config_file)
+    elif args.action == "evulate":
+        evulate(args.config_file, args.model_path)
+    else:
+        print("Undefined value")
+
+elif args.model_arch == "bert":
+    from scripts.train_bert import train
+    from evulating.evulate_bert import evulate
+
+    if args.action == "train":
+        train(args.config_file)
